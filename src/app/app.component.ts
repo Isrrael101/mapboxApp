f72ad342f57defcb179e621992c234184aec3645
Isrrael101 <isrraelfcaq@hotmail.com>
2021-10-16 23:01:58 -0400

    última modificación

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,31 @@ import * as Mapboxgl from 'mapbox-gl';
 })
 export class AppComponent implements OnInit{
 
-  mapa: Mapboxgl.Map;
+  mapa!: Mapboxgl.Map;
 
   ngOnInit() {
     
-    Mapboxgl.accessToken = environment.mapboxKey;
-
     this.mapa = new Mapboxgl.Map({
+      accessToken: environment.mapboxKey,
       container: 'mapa-mapbox', // container ID
       style: 'mapbox://styles/mapbox/streets-v11', // style URL
-      center: [-74.5, 40], // starting position
-      zoom: 9 // starting zoom
+      center: [-75.76, 45.35], // starting position
+      zoom: 16.6 // starting zoom
     });
+
+    this.crearMarcador(-75.76, 45.35);
+
+  }
+
+  crearMarcador(lng: number, lat: number){
+    const marker = new Mapboxgl.Marker({
+      draggable: true
+    })
+    .setLngLat([lng, lat])
+    .addTo(this.mapa);
+
+    marker.on('drag', () =>{
+      console.log(marker.getLngLat())
+    })
   }
 }
